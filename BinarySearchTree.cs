0e4d34a988445709621807398061c572f2b82203
AparnaKumariMishra <113542054+AparnaKumariMishra@users.noreply.github.com>
2022-10-22 03:09:11 +0530

    Add files via upload
    Adding BST2

---
+++ BinarySearchTree.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySearchTree
+{
+    internal class BinarySearchTree
+    {
+
+        public class Node
+        {
+            public int Data;
+            public Node Left;
+            public Node Right;
+            public void DisplayNode()
+            {
+                Console.Write(Data + " ");
+            }
+        }
+        public Node root;
+        public BinarySearchTree()
+        {
+            root = null;
+        }
+        public void Insert(int i)
+        {
+            Node newNode = new Node();
+            newNode.Data = i;
+            if (root == null)
+                root = newNode;
+            else
+            {
+                Node current = root;
+                Node parent;
+                while (true)
+                {
+                    parent = current;
+                    if (i < current.Data)
+                    {
+                        current = current.Left;
+                        if (current == null)
+                        {
+                            parent.Left = newNode;
+                            break;
+                        }
+
+                        else
+                        {
+                            current = current.Right;
+                            if (current == null)
+                            {
+                                parent.Right = newNode;
+                                break;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        static void Main()
+        {
+            BinarySearchTree nums = new BinarySearchTree();
+            nums.Insert(56);
+            nums.Insert(30);
+            nums.Insert(70);
+            nums.Insert(60);
+            nums.Insert(95);
+            nums.Insert(65);
+            nums.Insert(63);
+            nums.Insert(67);
+            nums.Insert(30);
+            nums.Insert(22);
+            nums.Insert(40);
+            nums.Insert(11);
+            nums.Insert(16);
+            nums.Insert(13);
+        }
+    }
+
+}
+
