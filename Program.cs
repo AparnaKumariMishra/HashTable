94017f5f5592eac01b2adfa280ab8c330688f0a9
AparnaKumariMishra <113542054+AparnaKumariMishra@users.noreply.github.com>
2022-10-22 03:07:49 +0530

    Add files via upload
    Adding BST program

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinaryTree
+{
+    internal class program
+    {
+        public static void Main()
+        {
+
+            UC1Add<int> Add = new UC1Add<int>(56);
+            Add.Insert(30);
+            Add.Insert(70);
+            Add.Display();
+            Console.ReadLine();
+
+        }
+
+    }
+}
